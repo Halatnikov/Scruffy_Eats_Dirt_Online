b6a26d455895adfd994834b2da096d09afddb760
Halatnikov <43994658+Halatnikov@users.noreply.github.com>
2024-04-02 20:33:52 +0700

    Initial commit 2: the squeakuel

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1712064585,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png",
+		"sprite2-sheet1.png",
+		"meatscruffy.ogg",
+		"sfx_chomp.ogg",
+		"sfx_victory.ogg",
+		"hi_kappa.ogg",
+		"msc_normal.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"comicneue-regular.woff2",
+		"font.css",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
